87cc4583a1083e361d920092092443541bab3d07
shankars25 <sutarshankar420@gmail.com>
2025-02-14 15:00:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,9 +14,10 @@ ALLOWED_EXTENSIONS = {'pdf', 'jpg', 'jpeg', 'png', 'mp3', 'xlsx', 'xls', 'txt'}
 DB_PATH = "files.db"
 
 def get_db_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def init_db():
     """Initialize the SQLite database."""
