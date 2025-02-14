db512e72b7d1df97331093e025d64ee0d5641fa4
shankars25 <sutarshankar420@gmail.com>
2025-02-14 11:25:28 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,18 @@
 from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://sutarshankar420:Admin@clusterddas.2r0vh.mongodb.net/")
+
+client = MongoClient(MONGO_URI)
+db = client["yourdb"]
+collection = db["your_collection"]
 
 def get_database():
     # Replace with your MongoDB connection URI
     client = MongoClient("mongodb://localhost:27017")
     db = client["ddas1"]
     return db
+
+
+
+
